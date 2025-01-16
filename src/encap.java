44a1a6977b32a09d9f89f8d384217324eeaf0a05
NAYAN ACHARYA <nayan374@proton.me>
2025-01-16 19:54:23 +0545

    encapsulation

---
+++ src/encap.java
@@ -0,0 +1,11 @@
+public class encap {
+    private void fun() {
+        System.out.println("This code shows us simple example of encapsulation");
+    }
+
+    public static void main(String args[]) {
+        encap obj = new encap();
+        obj.fun();
+    }
+
+}
