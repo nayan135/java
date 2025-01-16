0d952890f16fa4770a1a70e7eb6c52b7fda4a596
NAYAN ACHARYA <nayan374@proton.me>
2025-01-16 21:46:45 +0545

    matrix

---
+++ src/matrixx.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Matrix {
+    int[][] data;
+
+    void inputData() {
+        Scanner na = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the size of the matrix: ");
+        n = na.nextInt();
+        data = new int[n][n];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Enter number at [" + i + "][" + j + "]:");
+                data[i][j] = na.nextInt();
+            }
+        }
+    }
+
+    void sort() {
+        int n = data.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                for (int k = 0; k < n - j - 1; k++) {
+                    if (data[i][k] > data[i][k + 1]) {
+                        int temp = data[i][k];
+                        data[i][k] = data[i][k + 1];
+                        data[i][k + 1] = temp;
+                    }
+                }
+            }
+        }
+        System.out.println("Sorted matrix is: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Matrix m = new Matrix();
+        m.inputData();
+        m.sort();
+    }
+}
