100dd527fc3adc947f043178112fcf6471db29a1
NAYAN ACHARYA <nayan374@proton.me>
2025-01-16 20:48:32 +0545

    constructor

--- src/constructor.java
+++ src/constructor.java
@@ -1,18 +1,10 @@
 public class constructor {
-  constructor(String name){
-    System.out.println("the name ofthe student is:"+name);
-    
+  constructor(String name) {
+    System.out.println("the name of the student is:" + name);
+
   }
-    
+
+  public static void main(String[] args) {
+    constructor c = new constructor("NAYAN");
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
