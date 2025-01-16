00bd737ed888d305b5cf05f9f8b76b91e2821976
NAYAN ACHARYA <nayan374@proton.me>
2025-01-16 20:12:14 +0545

    polymorphism

---
+++ src/polymorphism.java
@@ -0,0 +1,15 @@
+public class polymorphism {
+    void add(int a, int b) {
+        System.out.println("Sum of two numbers is : " + (a + b));
+    }
+
+    void add(int a, int b, int c) {
+        System.out.println("Sum of three digits is: " + (a + b + c));
+    }
+
+    public static void main(String args[]) {
+        polymorphism obj = new polymorphism();
+        obj.add(10, 20);
+        obj.add(10, 20, 30);
+    }
+}
