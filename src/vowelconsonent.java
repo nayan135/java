b7f4b988208e97bba9233d57b47224bcd022d9ed
NAYAN ACHARYA <nayan374@proton.me>
2025-06-01 17:00:55 +0545

    naya aayo hai taaa

--- src/vowelconsonent.java
+++ src/vowelconsonent.java
@@ -11,16 +11,22 @@ public class vowelconsonent {
         ch = input.toCharArray();
     }
 
+    int count = 0;
+    int count2 = 0;
+
     void vowelcheck() {
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' ||
                     ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
                 System.out.println(ch[i] + " is a vowel");
+                count++;
             } else {
                 System.out.println(ch[i] + " is a consonant");
+                count2++;
             }
         }
-
+        System.out.println("Total vowels: " + count);
+        System.out.println("Total consonants: " + count2);
     }
 
     public static void main(String[] args) {
